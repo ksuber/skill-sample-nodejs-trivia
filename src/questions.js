53fecaeeddbef0c2e1256b7482d6ff6b6b48a4c9
ksuber <k_suber52@yahoo.com>
2017-02-19 18:52:07 -0500

    Update questions.js

--- src/questions.js
+++ src/questions.js
@@ -7,9 +7,7 @@ module.exports = {
      */
     "QUESTIONS_EN_GB" : [
         {
-            "A community-based study of a program to increase physical activity is conducted, and the
-findings are evaluated. A small p-value with an estimate is reported. Which of the following
-is the best interpretation of this result?": [
+            "A community-based study of a program to increase physical activity is conducted, and the findings are evaluated. A small p-value with an estimate is reported. Which of the following is the best interpretation of this result?": [
                 "It is unlikely the estimate differs from the null value because of random variability ",
                 "It is likely the estimate differs from the true value because of bias.",
                 "It is likely the estimate differs randomly and systematically from the norm.",
@@ -18,23 +16,50 @@ is the best interpretation of this result?": [
         },
         {
             "Which of the following statements best describes an intent-to-treat analysis": [
-                "Analyses maintain the original randomized assignment of treatments in the definition of
-intervention and control groups",
-                "United States",
-                "Analyses compare characteristics of participants who did and did not adhere to the
-randomized treatment",
-                "Analyses exclude all participants who did not adhere to the assigned randomized
-treatment.",
-                "Analyses reorganize participants into intervention and control groups based on their
-actual participation",            ]
+                "Analyses maintain the original randomized assignment of treatments in the definition of intervention and control groups",
+                "Analyses compare characteristics of participants who did and did not adhere to the randomized treatment",
+                "Analyses exclude all participants who did not adhere to the assigned randomized treatment.",
+                "Analyses reorganize participants into intervention and control groups based on their actual participation",            ]
         },
         {
-            "The epidemic of methyl mercury poisoning in Minamata, Japan, in the 1950s illustrated
-contamination of which of the following?": [
+            "The epidemic of methyl mercury poisoning in Minamata, Japan, in the 1950s illustrated contamination of which of the following?": [
                 "fish",
                 "water",
                 "soil",
                 "feed grain"
             ]
         },
-        };
+         {
+            "Only buy the the following products?": [
+                "Spacely Sprokets",
+                "Cogswell Cogs",
+                "Acme",
+                "Wayne Technologies"
+            ]
+        },
+         {
+            "It was all a dream I use to read?": [
+                "Word up magazine",
+                "The source magazine",
+                "x.x.l magazine",
+                "the washingtn post"
+            ]
+        },
+        {
+            "Wu tang sword style is derived from?": [
+                "Shaolin finger style",
+                "five deadly venoms",
+                "drunk monk style",
+                "water technique"
+            ]
+        },
+         {
+            "What is the ancient name for Egypt?": [
+                "Kemet",
+                "Carthedge",
+                "Ethopia",
+                "Kush"
+            ]
+        },
+        ]
+}
